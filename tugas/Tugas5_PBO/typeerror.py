c731385f7066adf9334dc306a2dd182b5461885c
sasonoadi <127710146+sasonoadi@users.noreply.github.com>
2023-07-15 22:07:54 +0700

    Add files via upload

---
+++ tugas/Tugas5_PBO/typeerror.py
@@ -0,0 +1,5 @@
+try:
+    x = "H"
+    y = x + 5
+except TypeError:
+    print("Terjadi kesalahan tipe data, pastikan variabel yang digunakan sudah benar!")
---
+++ tugas/Tugas5_PBO/zerodivisionerror.py
@@ -0,0 +1,5 @@
+try:
+    x = 1
+    y = x / 0
+except ZeroDivisionError:
+    print("Terjadi kesalahan pembagian dengan nol!")
